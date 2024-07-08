20362eb98e3f69f9188657ee0883af7c1468d99c
Shubhansu kumar <shubhansu2021@gmail.com>
2024-07-08 22:29:02 +0530

    Variables in Java

---
+++ Learning/02_Unit3/01_Variable.java
@@ -0,0 +1,35 @@
+// Here is a list of keywords in the Java programming language. You cannot use any of the following as identifiers in your programs. The keywords const and goto are reserved, even though they are not currently used. true, false, and null might seem like keywords, but they are actually literals; you cannot use them as identifiers in your programs.
+
+// abstract	continue	for	new	switch
+// assert***	default	goto*	package	synchronized
+// boolean	do	if	private	this
+// break	double	implements	protected	throw
+// byte	else	import	public	throws
+// case	enum****	instanceof	return	transient
+// catch	extends	int	short	try
+// char	final	interface	static	void
+// class	finally	long	strictfp**	volatile
+// const*	float	native	super	while
+
+// Variables are a way to store information in our computer
+
+// start jshell by typing 'jshell' in command prompt 
+// int number = 21;
+
+// int represents a integer, number is the name of the variable and 21 is the initial value of number variable
+
+// Task : Print the value of variable in jshell 
+// Solution : System.out.println(number);
+
+// Task : Change the value of variable to some other value
+// Solution: number = 7;
+// Solution: System.out.println(number);
+
+// use '/list' command to list out all the instructions you executed during the session.
+
+// Output : 
+// 1 : int number = 21;
+// 2 : System.out.println(number);
+// 3 : number = 7;
+// 4 : System.out.println(number);
+
