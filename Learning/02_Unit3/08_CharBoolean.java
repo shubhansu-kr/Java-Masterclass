5809cc12df56ba1e6996e962f37167a763fdec14
Shubhansu kumar <shubhansu2021@gmail.com>
2024-07-08 23:39:02 +0530

    Char and Boolean

---
+++ Learning/02_Unit3/08_CharBoolean.java
@@ -0,0 +1,18 @@
+// Char holds only one character and enclosed in a single quote
+
+/* 
+PS C:\Users\12345> jshell
+|  Welcome to JShell -- Version 21.0.2
+|  For an introduction type: /help intro
+
+jshell> char myChar = 'A'
+myChar ==> 'A'
+ */
+
+//  A char takes up 16 bits
+// we can use unicode representation to initialise a variable with a backslash.
+
+// char mynewchar = '\u0044';
+
+
+// Boolean holds only two values : true and false ;
