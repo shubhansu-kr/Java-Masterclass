708f3cacbad69b3009ca0346669017404aecf1a9
Shubhansu kumar <shubhansu2021@gmail.com>
2024-07-08 22:35:51 +0530

    Expression in java

---
+++ Learning/02_Unit3/02_Expression.java
@@ -0,0 +1,43 @@
+// Task : Create two additional variable and assign their product to first variable
+// Solution: 
+/* 
+
+1 : int number = 21;
+2 : System.out.println(number);
+3 : number = 7;
+4 : System.out.println(number);
+5 : number = 10 + 11 ;
+6 : System.out.println(number);
+7 : int firstNumber = 21;
+8 : int secondNumber = 7;
+9 : int thirdNumber = firstNumber * secondNumber; 
+
+*/
+
+// '/var' command is used to list all the initialised varible.
+/* 
+|    int number = 21
+|    int firstNumber = 21
+|    int secondNumber = 7
+|    int thirdNumber = 147
+*/ 
+
+// Task : Create a variable a fourthVariable and initialse to 1000, reduce the total val of all the prev variable from it
+// Solution
+/* 
+   1 : int number = 21;
+   2 : System.out.println(number);
+   3 : number = 7;
+   4 : System.out.println(number);
+   5 : number = 10 + 11 ;
+   6 : System.out.println(number);
+   7 : int firstNumber = 21;
+   8 : int secondNumber = 7;
+   9 : int thirdNumber = firstNumber * secondNumber;
+  10 : int fourthVariable = 1000;
+  11 : int newDiff = fourthVariable - number - firstNumber - secondNumber - thirdNumber;
+  12 : System.out.println(newDiff);
+ */
+
+//  use 'tab' to auto complete the variable names;
+
